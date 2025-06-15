5acd79cd12a8497fcd44783227045f12d5025e02
Muhammad Hamza <muhammadhamzao241@gmail.com>
2025-06-15 10:32:50 +0500

    Update intro_hamza.py

--- intro_hamza.py
+++ intro_hamza.py
@@ -10,27 +10,27 @@ Headers={
     'Authorization':f'Bearer {api_key}',
     'Content-Type':'application/json'
 }
-loader=TextLoader('hamza-port.txt')
+loader=TextLoader('D:/Code/hamza-port.txt')
 document=loader.load()
 splitter=RecursiveCharacterTextSplitter(chunk_size=500,chunk_overlap=10)
 chunks=splitter.split_documents(document)
 embeddingss=HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
 vectorstore=Chroma.from_documents(chunks,embeddingss)
-while True:
-    user=str(input("User: "))
-    query=user
-    similar=vectorstore.similarity_search(query,k=3)
-    context=[main.page_content for main in similar]
-    prompt=f"""Your name is Hamza and when someone ask quesion to you which make sure you reply from Given Context. You are funny too!
+
+user=st.text_input("User: ")
+query=user
+similar=vectorstore.similarity_search(query,k=3)
+context=[main.page_content for main in similar]
+prompt=f"""Your name is Hamza and when someone ask quesion to you which make sure you reply from Given Context. You are funny too!
 Context: {context}
 Query: {query}"""
-    payload={
+payload={
     'model':'llama3-8b-8192',
     'messages':[{
         'role':'user' ,'content' :prompt
     }],
     'max_tokens':60
 }
-    response=requests.post(url=url,headers=Headers,json=payload)
-    data=response.json()
-    print(f'Hamza: {data["choices"][0]["message"]["content"]}')
+response=requests.post(url=url,headers=Headers,json=payload)
+data=response.json()
+st.write(f'Hamza: {data["choices"][0]["message"]["content"]}')
